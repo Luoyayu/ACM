689e5ecec77d52987e3b5543e0ba872366f85b41
Luoyayu <gavinhty@hotmail.com>
2017-05-19 19:15:38 +0800

    HDU1104

--- CodeBook/数论/CRT/HDU4542小明系列故事——未知剩余系.cpp
+++ CodeBook/数论/CRT/HDU4542小明系列故事——未知剩余系.cpp
@@ -1,14 +1,14 @@
 ///如果type==0即是求出整数的约数为K
 ///如果type==1即是
-#include <iostream>
 #include <cstdio>
 using namespace std;
-int n;
-const int maxn=50000;
-typedef unsigned long long LL;
-LL ans,ip[maxn];
+int k;
+const int maxn=50000+10;
+typedef __int64 LL;
+LL ans;
+int ip[maxn];
 const LL INF=((LL)1<<62)+1;
-int prime[]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
+int p[]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
 void prim()
 {
     for(int i=1;i<=maxn;i++)
@@ -16,35 +16,35 @@ void prim()
         ip[i]+=i;
         for(int j=i;j<=maxn;j+=i)
             ip[j]--;
-
         if(!ip[ip[i]])
             ip[ip[i]]=i;
         ip[i]=0;
     }
 }
-void dfs(int num,LL tmp,int dept)
+void dfs(int num,LL t,int pos)
 {
-    if(num==n) ans=min(tmp,ans);
-    if(num>=n || (dept>=16)) return ;
+    if(num==k && ans>t) ans=t;
+    if(num>=k || (pos>=15)) return ;
     for(int i=1; i<=62; i++)
     {
-        if(tmp>ans/prime[dept]) return ;
-
-        tem*=prime[dept];
-            dfs(num*(i+1), tmp, dept+1);
+        if(t>ans/p[pos]||num*(i+1)>k) return; ///不剪枝会wa
+        t *= p[pos];
+        if(k%(num*(i+1)) == 0)
+            dfs(num*(i+1), t, pos+1);
     }
 }
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    int t;scanf("%d",&t);
+    int t;
+    scanf("%d",&t);
     prim();
     for(int kase=1;kase<=t;kase++)
     {
         int type;
-        cin>>type>>n;
+        scanf("%d %d",&type,&k);
         if(type)
-            ans=ip[n];
+            ans=ip[k];
         else
         {
             ans = INF;
@@ -52,11 +52,11 @@ int main()
         }
         printf("Case %d: ",kase);
         if(ans==0)
-            cout<<"Illegal"<<endl;
+            printf("Illegal\n");
         else if(ans>=INF)
-            cout<<"INF"<<endl;
+            printf("INF\n");
         else
-            cout<<ans<<endl;
+            printf("%I64d\n",ans);
     }
     return 0;
 }
---
+++ CodeBook/数论/HDU1104Remainder.cpp
@@ -0,0 +1,82 @@
+///2017年5月19日17点13分
+///给定三个整数 N,M,K 对于N有三种操作，N+M,N-M,N*M,N%M;
+///问怎么样最少操作使得(起始N+1)%K==(现在N)%K
+///注意此处的mod模后的结果都为正,故A%B=(A%B+B)%B;
+///由HDU4474的结论可知,模数相等的情况下对两个数操作结果是一致的
+#include<cstdio>
+#include<vector>
+#include<string>
+#include<queue>
+#include<iostream>
+#include<cstring>
+int mod;
+int n,m,k;
+const int maxn=1000000+555;
+bool vis[maxn];
+using namespace std;
+struct node
+{
+    int num;
+    int step;
+    string road;
+}q1,now;
+void bfs()
+{
+    memset(vis,0,sizeof vis);
+    queue<node> q;
+    q1.num=n,q1.step=0,q1.road="";
+    q.push(q1);
+    vis[(n%k+k)%k]=1;//标记该余数
+    while(!q.empty())
+    {
+        now = q.front();q.pop();
+        if(((n+1)%k+k)%k == (now.num%k+k)%k)
+        {
+            printf("%d\n",now.step);
+            cout<<now.road<<endl;
+            return;
+        }
+        q1.step=now.step+1;
+        for(int i=0;i<4;i++)
+        {
+            if(i==0)
+            {
+                q1.num = (now.num+m)%mod;
+                q1.road = now.road+'+';
+            }
+            else if(i==1)
+            {
+                q1.num = (now.num-m)%mod;
+                q1.road = now.road+'-';
+            }
+            else if(i==2)
+            {
+                q1.num = (now.num*m)%mod;
+                q1.road = now.road+'*';
+            }
+            else
+            {
+                q1.num = (now.num%m+m)%m%mod;
+                q1.road = now.road+'%';
+            }
+
+            if(!vis[(q1.num%k+k)%k])
+            {
+                q.push(q1);
+                vis[(q1.num%k+k)%k]=1;
+            }
+        }
+    }
+    printf("0\n");
+}
+int main()
+{
+    while(scanf("%d%d%d",&n,&k,&m)!=EOF,(n||k||m))
+    {
+        mod=k*m;//由于有操作N%M,对于N%N%K是错误的,故每次mod (N*K)稳
+        bfs();
+    }
+    return 0;
+}
+
+
