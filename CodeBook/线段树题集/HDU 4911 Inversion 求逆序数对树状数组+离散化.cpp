85c30e601bb55ddd16850d4fbef2b514ed9d2a26
Luoyayu <gavinhty@hotmail.com>
2017-06-06 17:25:35 +0800

    cccccc

--- CodeBook/线段树题集/HDU 4911 Inversion 求逆序数对树状数组+离散化.cpp
+++ CodeBook/线段树题集/HDU 4911 Inversion 求逆序数对树状数组+离散化.cpp
@@ -1,10 +1,9 @@
 //数据的离散化/逆序数和==swap次数
 #include<cstdio>
 #include<cstring>
-#include<iostream>
 #include<algorithm>
 using namespace std;
-typedef __int64 ll;
+typedef long long ll;
 const int maxn=1000005;
 ll c[maxn];
 int n;
@@ -12,16 +11,21 @@ struct node
 {
     ll x,index;
 }s[maxn];
-inline ll lowbit(int x)
+bool cmp(node &a, node &b)
+{
+    return a.x<b.x;
+}
+ ll lowbit(ll x)
 {
     return x&(-x);
-    }
-inline void add(ll x,ll d)
+}
+
+ void add(ll x,ll d)
 {
     while(x<=n)
         c[x]+=d,x+=lowbit(x);
 }
-ll sum(int x)
+ll sum(ll x)
 {
     ll ret=0;
     while(x)
@@ -32,15 +36,15 @@ int main()
 {
     while(scanf("%d",&n)!=EOF)
     {
-        ll k;scanf("%lld",&k);
+        //ll k;scanf("%lld",&k);
         memset(c,0,sizeof c);
         for(ll i=1;i<=n;i++)
         {
-            scanf("%I64d",&s[i].x);
+            scanf("%lld",&s[i].x);
             s[i].index=i;//记录x对应的索引
         }
 
-        sort(s+1,s+n+1);//将有序数列索引与原数列索引对比
+        sort(s+1,s+n+1,cmp);//将有序数列索引与原数列索引对比
 
         ll ret=0;//交换次数
         for(ll i=1;i<=n;i++)
@@ -49,8 +53,8 @@ int main()
             ret+=i-sum(s[i].index);//sum(i)表示比i小的个数，i-sum(i)即表示比i大的个数
         }
 
-        if(ret==k) ret=k;
-        printf("%I64d\n",ret-k);
+        //if(ret==k) ret=k;
+        printf("%lld\n",ret);
     }
     return 0;
 }
