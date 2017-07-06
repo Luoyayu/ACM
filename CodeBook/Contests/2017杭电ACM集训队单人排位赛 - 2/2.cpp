cbdef00c58af7d3fbe9a561646b6f9c877be35a7
Luoyayu <gavinhty@hotmail.com>
2017-07-06 11:05:36 +0800

    补题

--- CodeBook/Contests/2017杭电ACM集训队单人排位赛 - 2/2.cpp
+++ CodeBook/Contests/2017杭电ACM集训队单人排位赛 - 2/2.cpp
@@ -1,1 +1,33 @@
+#include<bits/stdc++.h>
+//数学问题——Simpson's rule
+using namespace std;
+double v1, v2, x, k;
+const double esp = 1e-6;
+inline double f(double t)
+{
+    return k / (pow ( hypot(v1*t, x-v2*t), 2) );
+}
+inline double Simpson(double a, double b)
+{
+    double mid = a+(b-a)/2;
+    return ( f(a) + 4 * f(mid) + f(b) ) * (b-a) / 6;
+}
+double ASR(double a, double b)
+{
+    double sum = Simpson(a,b);
+    double mid = a+(b-a)/2;
+    double suma = Simpson(a,mid);
+    double sumb = Simpson(mid,b);
+    return (fabs(sum-suma-sumb)<esp?sum:ASR(a,mid)+ASR(mid,b));
+}
+int main(int argc, char const *argv[])
+{
+    int t;scanf("%d\n", &t);
+    while (t--)
+    {
+        scanf("%lf%lf%lf%lf", &v1, &v2, &x, &k);
+        printf("%.2f\n", ASR(0, 20000));
+    }
 
+    return 0;
+}
--- CodeBook/Contests/2017杭电ACM集训队单人排位赛 - 2/6.cpp
+++ CodeBook/Contests/2017杭电ACM集训队单人排位赛 - 2/6.cpp
@@ -1,8 +1,34 @@
+/*长度为n的花环,有k种花,相邻的花不同,第l和第r朵花颜色相同,求方案数mod 1e9+7 l,r重要的是长度 i
+* dp 固定一端考虑另一端增长的情况,设dp[i][1]为长度为i,第i朵花与固定端相同的方案数,
+* dp[i][0]表示长度为i,第i朵花与固定段不同的方案数
+*/
 #include<bits/stdc++.h>
 #define Debug(x) cout<<#x<<"= "<<x<<endl;
 using namespace std;
-const int maxn=1e6+7;
+const int mod = 1e9 + 7;
+const int maxn = 1e5 + 7;
+typedef long long LL;
+int n,m,k,l,r;
+LL dp[maxn][2];
 int main()
 {
-
+    while(scanf("%d%d%d",&n, &m, &k)!=EOF)
+    {
+        dp[1][0]=0,dp[1][1]=k-1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i][1]=(dp[i-1][1]*(k-2)%mod+dp[i-1][0]*(k-1)%mod)%mod;
+            dp[i][0]=dp[i-1][1];//长度为i的且与固定端颜色相同的个数由长度为i-1与固定端不同递推来
+        }
+        while(m--)
+        {
+            scanf("%d%d",&l,&r);
+            if(l>r) swap(l,r);
+            int t1 = r-l-1;
+            int t2 = n-t1-2;
+            LL ans = (k*dp[t1][1]%mod*(dp[t2][1]%mod))%mod;
+            printf("%I64d\n",ans);
+        }
+    }
+    return 0;
 }
