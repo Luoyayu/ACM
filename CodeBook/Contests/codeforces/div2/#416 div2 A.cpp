910f0f9b629f65d8d0556bd5eeb246d80d34bc4d
luoyayu <gavinhty@gmail.com>
2018-03-06 15:39:40 +0800

    update linux cppincludepath

--- CodeBook/Contests/codeforces/div2/#416 div2 A.cpp
+++ CodeBook/Contests/codeforces/div2/#416 div2 A.cpp
@@ -1,23 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int a,b;
-    while(scanf("%d %d",&a,&b)!=EOF)
+    int a, b;
+    while (scanf("%d %d", &a, &b) != EOF)
     {
-        int cnt=1;
-        int turn=0;
-        while(a>=0&&b>=0)
+        int cnt = 1;
+        int turn = 0;
+        while (a >= 0 && b >= 0)
         {
-            if(turn)
-                a-=cnt;
+            if (turn)
+                a -= cnt;
             else
-                b-=cnt;
-            turn^=1;
+                b -= cnt;
+            turn ^= 1;
             cnt++;
         }
-        if(a>=0) printf("Vladik\n");
-        else printf("Valera\n");
+        if (a >= 0)
+            printf("Vladik\n");
+        else
+            printf("Valera\n");
     }
     return 0;
 }
